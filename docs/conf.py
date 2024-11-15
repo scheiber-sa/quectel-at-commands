10695ed4de8735a61470d0c75cc9a6cf59e23bbe
Pierre Godicheau <pgodicheau@scheiber.fr>
2024-11-15 11:53:55 +0100

    update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,25 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
+import configparser
 
+# Add project root to sys.path
 sys.path.insert(0, os.path.abspath(".."))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "quectelatcommands"
-copyright = "2024, Pierre Godicheau"
-author = "Pierre Godicheau"
-release = "1.0.0"
+copyright = copyright = (
+    "2024, Pierre Godicheau. Licensed under the "
+    "GNU General Public License v3.0. See https://www.gnu.org/licenses/gpl-3.0.html for details."
+)
+
+# Fetch version dynamically from setup.cfg
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), "..", "setup.cfg"))
+project = config["metadata"]["name"]
+author = config["metadata"]["author"]
+release = config["metadata"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -27,7 +37,6 @@ extensions = [
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
